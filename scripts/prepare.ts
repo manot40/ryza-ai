b1073be0dc58b0dac1cb2d44b0e8dc0e62164f3f
Kevin Sandi Ho <kevin.sandi@citis.kompasgramedia.com>
2026-09-18 16:46:19 +0700

    Fix: include web artifacts

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,20 +1,48 @@
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
+import { exec } from 'node:child_process';
+
 import pkg from '../package.json';
 
 const VERSION = pkg.version;
-const REPO_ASSETS_URL = 'https://github.com/zeroa234/ryza-ai-revive/archive/refs/tags';
+const VERSION_LOCAL = './web/assets/VERSION';
+const TMP_FILE = path.join(os.tmpdir(), `ryza-${VERSION}.zip`);
+const RELEASE_URL = 'https://github.com/zeroa234/ryza-ai-revive/releases';
 
-console.info('Downloading web assets for version', VERSION);
+if (fs.existsSync(VERSION_LOCAL)) {
+  const version = fs.readFileSync(VERSION_LOCAL, 'utf8').trim();
+  if (version === VERSION) {
+    console.info('Current local assets already updated. Assets extraction skipped.');
+    process.exit(0);
+  }
+}
 
 try {
-  const res = await fetch(`${REPO_ASSETS_URL}/v${pkg.version}.zip`);
+  const file = Bun.file(TMP_FILE);
+  const res = await fetch(`${RELEASE_URL}/download/v${pkg.version}/RyzaChat-${VERSION}.apk`);
   if (!res.ok) {
-    console.error('Cannot get upstream archive with version', VERSION, 'Error:', res.statusText);
+    console.error('Cannot get upstream release with version', VERSION, 'Error:', res.statusText);
     process.exit(1);
   }
 
-  const archive = new Bun.Archive(await res.blob());
-  await archive.extract('./web', { glob: 'web/**' });
+  console.info('Downloading web assets for version', VERSION);
+  await file.write(res);
   console.info('Web assets download completed!');
+
+  console.info('Extracting assets...');
+  exec(`unzip ${TMP_FILE} -d _extracted assets/assets/**`, (err) => {
+    if (err) throw err;
+    if (fs.existsSync('./web/assets'))
+      // prettier-ignore
+      fs.rmSync('./web/assets', { force:true, recursive: true });
+
+    fs.renameSync('_extracted/assets/assets', 'web/assets');
+    fs.rmSync('_extracted', { recursive: true });
+    fs.rmSync(TMP_FILE, { force: true });
+    fs.writeFileSync(VERSION_LOCAL, VERSION);
+    console.info('Assets extracted into web/assets');
+  });
 } catch (e: any) {
   console.error('Cannot process downloaded archive. Error:', e.message);
 }
